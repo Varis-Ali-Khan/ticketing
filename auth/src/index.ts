ea61470c3f9fa4489b9f377d73ab58afcc128fcd
API2296 <93130822+API2296@users.ngithuboreply..com>
2023-10-05 11:51:40 +0530

    addes startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+
+  console.log("hi varis ali khan ")
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
